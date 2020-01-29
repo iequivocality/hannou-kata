20878024118b47c2b18e26a3ab6aa6e7c65340e7
iequivocality <xanthiandere@gmail.com>
2020-01-30 00:08:21 +0800

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from './store/reducers';
 import { Provider } from 'react-redux';
 import { logger } from 'redux-logger';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const store = createStore(rootReducer, applyMiddleware(logger));
 
@@ -11,10 +17,52 @@ const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="App">
-        
-      </div>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+        </div>
     </Provider>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
